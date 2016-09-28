267331a24ec0be5f3986cfab388f2a7ee669ec4d
QUAGMIRE <league@WTS11.attlocal.net>
2016-09-27 18:31:45 -0700

    hello

---
+++ src/ForLoopFromScratch.java
@@ -0,0 +1,24 @@
+
+public class ForLoopFromScratch {
+public static void main(String[] args) {
+	
+
+
+
+for(int i=1; i<4; i++){
+	
+	System.out.println(i+ "potatoe");
+	
+}
+System.out.println("4");
+
+for(int j=5; j<8; j++){
+System.out.println(j+ "potatoe");
+
+}
+
+
+System.out.println("more");
+
+}
+}
